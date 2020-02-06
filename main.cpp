518655bf2c0a287a3d4c1745e87b1d4c7fbf6148
SongHayoung <lovelydays95@naver.com>
2020-02-06 17:34:09 +0900

    1:00:00

--- main.cpp
+++ main.cpp
@@ -1,165 +1,70 @@
 #include <iostream>
-#include <queue>
-#include <stdlib.h>
-#include <memory.h>
+#include <vector>
 using namespace std;
-queue<pair<char,char>> q;
-void swap(char &a, char&b, char&c, char&d){
-    char temp = a;
-    a = d;
-    d = c;
-    c = b;
-    b = temp;
+int N;
+long long answer = 0;
+
+void print(vector<vector<int>> board) {
+    for (int i = 0; i < N+1; i++) {
+        for (int j = 0; j < N+1; j++)
+            cout << board[i][j] << " ";
+        cout << endl;
+    }
+    cout << endl;
 }
-
-void play(){            // 0  1   2      3   4   5       6   7   8
-    char cube[12][9] =  {{' ',' ',' ',  'o','o','o',    ' ',' ',' '},   //0
-                         {' ',' ',' ',  'o','o','o',    ' ',' ',' '},   //1
-                         {' ',' ',' ',  'o','o','o',    ' ',' ',' '},   //2
-                         {'g','g','g',  'w','w','w',    'b','b','b'},   //3
-                         {'g','g','g',  'w','w','w',    'b','b','b'},   //4
-                         {'g','g','g',  'w','w','w',    'b','b','b'},   //5
-                         {' ',' ',' ',  'r','r','r',    ' ',' ',' '},   //6
-                         {' ',' ',' ',  'r','r','r',    ' ',' ',' '},   //7
-                         {' ',' ',' ',  'r','r','r',    ' ',' ',' '},   //8
-                         {' ',' ',' ',  'y','y','y',    ' ',' ',' '},   //9
-                         {' ',' ',' ',  'y','y','y',    ' ',' ',' '},   //10
-                         {' ',' ',' ',  'y','y','y',    ' ',' ',' '}};  //11
-    char c, o;
-    while(!q.empty()){
-        c=q.front().first;
-        o=q.front().second;
-        q.pop();
-        if(c=='U'){
-            if(o=='+'){
-                swap(cube[3][3],cube[3][5],cube[5][5],cube[5][3]);
-                swap(cube[3][4],cube[4][5],cube[5][4],cube[4][3]);
-
-                swap(cube[2][3],cube[3][6],cube[6][5],cube[5][2]);
-                swap(cube[2][4],cube[4][6],cube[6][4],cube[4][2]);
-                swap(cube[2][5],cube[5][6],cube[6][3],cube[3][2]);
-            }
-            else{
-                swap(cube[3][3],cube[5][3],cube[5][5],cube[3][5]);
-                swap(cube[3][4],cube[4][3],cube[5][4],cube[4][5]);
-
-                swap(cube[2][3],cube[5][2],cube[6][5],cube[3][6]);
-                swap(cube[2][4], cube[4][2], cube[6][4], cube[4][6]);
-                swap(cube[2][5], cube[3][2], cube[6][3], cube[5][6]);
-            }
-        }
-        else if(c=='F'){
-            if(o=='+'){
-                swap(cube[6][3],cube[6][5],cube[8][5],cube[8][3]);
-                swap(cube[6][4],cube[7][5],cube[8][4],cube[7][3]);
-
-                swap(cube[5][3],cube[5][6],cube[9][5],cube[5][0]);
-                swap(cube[5][4],cube[5][7],cube[9][4],cube[5][1]);
-                swap(cube[5][5],cube[5][8],cube[9][3],cube[5][2]);
-            }
-            else{
-                swap(cube[6][3],cube[8][3],cube[8][5],cube[6][5]);
-                swap(cube[6][4],cube[7][3],cube[8][4],cube[7][5]);
-
-                swap(cube[5][3],cube[5][0],cube[9][5],cube[5][6]);
-                swap(cube[5][4],cube[5][1],cube[9][4],cube[5][7]);
-                swap(cube[5][5],cube[5][2],cube[9][3],cube[5][8]);
-            }
-        }
-        else if(c=='D'){
-            if(o=='+'){
-                swap(cube[9][3],cube[9][5],cube[11][5],cube[11][3]);
-                swap(cube[9][4],cube[10][5],cube[11][4],cube[10][3]);
-
-                swap(cube[8][3],cube[5][8],cube[0][5],cube[3][0]);
-                swap(cube[8][4],cube[4][8],cube[0][4],cube[4][0]);
-                swap(cube[8][5],cube[3][8],cube[0][3],cube[5][0]);
-            }
-            else{
-                swap(cube[9][3],cube[11][3],cube[11][5],cube[9][5]);
-                swap(cube[9][4],cube[10][3],cube[11][4],cube[10][5]);
-
-                swap(cube[8][3],cube[3][0],cube[0][5],cube[5][8]);
-                swap(cube[8][4],cube[4][0],cube[0][4],cube[4][8]);
-                swap(cube[8][5],cube[5][0],cube[0][3],cube[3][8]);
-            }
-        }
-        else if(c=='B'){
-            if(o=='+'){
-                swap(cube[0][3],cube[0][5],cube[2][5],cube[2][3]);
-                swap(cube[0][4],cube[1][5],cube[2][4],cube[1][3]);
-
-                swap(cube[11][3],cube[3][8],cube[3][5],cube[3][2]);
-                swap(cube[11][4],cube[3][7],cube[3][4],cube[3][1]);
-                swap(cube[11][5],cube[3][6],cube[3][3],cube[3][0]);
-            }
-            else{
-                swap(cube[0][3],cube[2][3],cube[2][5],cube[0][5]);
-                swap(cube[0][4],cube[1][3],cube[2][4],cube[1][5]);
-
-                swap(cube[11][3],cube[3][2],cube[3][5],cube[3][8]);
-                swap(cube[11][4],cube[3][1],cube[3][4],cube[3][7]);
-                swap(cube[11][5],cube[3][0],cube[3][3],cube[3][6]);
-            }
-        }
-        else if(c=='L'){
-            if(o=='+'){
-                swap(cube[3][0],cube[3][2],cube[5][2],cube[5][0]);
-                swap(cube[3][1],cube[4][2],cube[5][1],cube[4][0]);
-
-                swap(cube[0][3],cube[3][3],cube[6][3],cube[9][3]);
-                swap(cube[1][3],cube[4][3],cube[7][3],cube[10][3]);
-                swap(cube[2][3],cube[5][3],cube[8][3],cube[11][3]);
-            }
-            else{
-                swap(cube[3][0],cube[5][0],cube[5][2],cube[3][2]);
-                swap(cube[3][1],cube[4][0],cube[5][1],cube[4][2]);
-
-                swap(cube[0][3],cube[9][3],cube[6][3],cube[3][3]);
-                swap(cube[1][3],cube[10][3],cube[7][3],cube[4][3]);
-                swap(cube[2][3],cube[11][3],cube[8][3],cube[5][3]);
-            }
-        }
-        else{
-            if(o=='+'){
-                swap(cube[3][6],cube[3][8],cube[5][8],cube[5][6]);
-                swap(cube[3][7],cube[4][8],cube[5][7],cube[4][6]);
-
-                swap(cube[0][5],cube[9][5],cube[6][5],cube[3][5]);
-                swap(cube[1][5],cube[10][5],cube[7][5],cube[4][5]);
-                swap(cube[2][5],cube[11][5],cube[8][5],cube[5][5]);
-            }
-            else{
-                swap(cube[3][6],cube[5][6],cube[5][8],cube[3][8]);
-                swap(cube[3][7],cube[4][6],cube[5][7],cube[4][8]);
-
-                swap(cube[0][5],cube[3][5],cube[6][5],cube[9][5]);
-                swap(cube[1][5],cube[4][5],cube[7][5],cube[10][5]);
-                swap(cube[2][5],cube[5][5],cube[8][5],cube[11][5]);
-            }
+vector<vector<int>> rotate(vector<vector<int>> b) {
+    vector<vector<int>> ret(N+1, vector<int>(N+1));
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            ret[j][N - i - 1] = b[i][j];
         }
     }
-
-    for(int i=3;i<6;i++) {
-        for (int j = 3; j < 6; j++)
-            cout << cube[i][j];
-        cout <<endl;
+    return ret;
+}
+void DFS(int depth, vector < vector <int>> board) {
+    if (depth <= 5) {
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                answer = answer < board[i][j] ? board[i][j] : answer;
+        if(depth==5)
+            return;
+    }
+    vector<vector<int>> _board(N+1, vector<int>(N+1));
+    vector<vector<int>> _board2(N+1, vector<int>(N+1));
+    _board = board;
+    for (int i = 0; i < 4; i++) {
+        _board2 = _board;
+        for (int j = 0; j < N; j++) {
+            int zero_cnt = 0;
+            for (int k = 0; k < _board2[j].size(); k++) {
+                if (_board2[j][k] == 0) {
+                    _board2[j].erase(_board2[j].begin() + k);
+                    --k;   ++zero_cnt;
+                }
+            }
+            for (int k = 0; k < zero_cnt; k++)
+                _board2[j].push_back(0);
+            for (int k = 0; k < _board2[j].size()-1; k++) {
+                if (_board2[j][k] == _board2[j][k + 1]&&_board2[j][k]!=0) {
+                    _board2[j][k] *= 2;
+                    _board2[j].erase(_board2[j].begin() + k + 1);
+                    _board2[j].push_back(0);
+                }
+            }
+        }
+        DFS(depth + 1, _board2);
+        _board = rotate(_board);
     }
 }
 
-int main(void) {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    int tc, n;
-    char c,o;
-    cin>>tc;
-    for(int test=0;test<tc;test++){
-        cin>>n;
-        for(int i=0;i<n;i++){
-            cin>>c>>o;
-            q.push(make_pair(c,o));
-        }
-        play();
-    }
+int main(void)
+{
+    cin >> N;
+    vector<vector<int>> board(N+1, vector<int>(N+1));
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            cin >> board[i][j];
+
+    DFS(0, board);
+    cout << answer << endl;
 }
