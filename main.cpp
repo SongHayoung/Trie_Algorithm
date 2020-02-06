635112a10155b3911f0be57926682cf8177c5676
SongHayoung <lovelydays95@naver.com>
2020-02-06 17:31:32 +0900

    26:34

--- main.cpp
+++ main.cpp
@@ -1,25 +1,61 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
-int main(void)
-{
-    int tc;
-    cin >> tc;
-    int t2, m2;
-    int at, am;
-    bool flag;
-    for (int i = 1; i <= tc; i++) {
-        cin >> at >> am >> t2 >> m2;
-        flag = false;
-        am += m2;
-        if (am >= 60) {
-            flag = true;
-            am -= 60;
+int dx[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
+int dy[8] = {-1,-1,-1,  0, 0,  1, 1, 1};
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int kx, ky;
+    int sx, sy;
+    int d, n;
+    char c;
+    cin>>c>>d;
+    kx=c-'A';   ky=8-d;
+    cin>>c>>d;
+    sx=c-'A';   sy=8-d;
+    cin>>n;
+    string cmd;
+    getline(cin, cmd);
+    int nkx, nky, nsx, nsy, Icmd;
+    for(int i=0;i<n;i++) {
+        getline(cin, cmd);
+        if (cmd == "LT")
+            Icmd = 0;
+        else if (cmd == "T")
+            Icmd = 1;
+        else if(cmd=="RT")
+            Icmd = 2;
+        else if(cmd=="L")
+            Icmd = 3;
+        else if(cmd=="R")
+            Icmd = 4;
+        else if(cmd=="LB")
+            Icmd = 5;
+        else if(cmd=="B")
+            Icmd = 6;
+        else if(cmd=="RB")
+            Icmd = 7;
+
+        nkx = kx + dx[Icmd];
+        nky = ky + dy[Icmd];
+        if (0 <= nkx && nkx <= 7 && 0 <= nky && nky <= 7) {
+            if (nkx == sx && nky == sy) {
+                nsx = sx + dx[Icmd];
+                nsy = sy + dy[Icmd];
+                if(0 <= nsx && nsx <= 7 && 0 <= nsy && nsy <= 7){
+                    sx = nsx; sy = nsy; ky = nky; kx = nkx;
+                }
+            }
+            else{
+                kx = nkx;   ky = nky;
+            }
         }
-        at += t2;
-        if (flag)
-            ++at;
-        if (at >= 13)
-            at -= 12;
-        cout << "#" << i << " " << at << " " << am << endl;
     }
+    c = kx+'A'; d = 8-ky;
+    cout<<c<<d<<endl;
+    c = sx+'A'; d = 8-sy;
+    cout<<c<<d<<endl;
 }
