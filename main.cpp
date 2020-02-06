936e7b6620bd56e40ccc5d1537f955d40dc4a711
SongHayoung <lovelydays95@naver.com>
2020-02-06 17:40:00 +0900

    31:44

--- main.cpp
+++ main.cpp
@@ -1,91 +1,32 @@
 #include <iostream>
-#include <memory.h>
-#include <queue>
-#include <algorithm>
-
+#include <vector>
 using namespace std;
-int R, C;
-int dx[4] = {0,1,0,-1};
-int dy[4] = {-1,0,1,0};
-int gx, gy;
-char MAP[50][50];
-
-void printMAP(){
-    for(int i=0;i<C;i++){
-        for(int j=0;j<R;j++){
-            cout<<MAP[i][j]<<" ";
-        }
-        cout<<endl;
-    }
-    cout<<endl<<endl;
-}
-
-queue<pair<int,int>> BFS(queue<pair<int,int>> q){
-    queue<pair<int,int>> nq;
-    while(!q.empty()){
-        int x = q.front().second;
-        int y = q.front().first;
-        q.pop();
-        for(int i=0;i<4;i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if(0<=nx&&nx<R && 0<=ny&&ny<C){
-                if(MAP[ny][nx]=='.'||MAP[ny][nx]=='S'){
-                    MAP[ny][nx]='*';
-                    nq.push(make_pair(ny,nx));
-                }
-            }
-        }
-    }
-    return nq;
-}
-queue<pair<int,int>> dBFS(queue<pair<int,int>> q){
-    queue<pair<int,int>> nq;
-    while(!q.empty()){
-        int x = q.front().second;
-        int y = q.front().first;
-        q.pop();
-        for(int i=0;i<4;i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if(0<=nx&&nx<R && 0<=ny&&ny<C){
-                if(MAP[ny][nx]=='.'||MAP[ny][nx]=='D'){
-                    MAP[ny][nx]='S';
-                    nq.push(make_pair(ny,nx));
-                }
-            }
-        }
-    }
-    return nq;
-}
 int main(void){
-    cin>>C>>R;
-    int x, y;
-    queue<pair<int,int>> q;
-    queue<pair<int,int>> dq;
-    for(int i=0;i<C;i++)
-        for(int j=0;j<R;j++){
-            cin>>MAP[i][j];
-            if(MAP[i][j]=='S') {
-                dq.push(make_pair(i,j));
-            }
-            else if(MAP[i][j]=='D'){
-                gx = j; gy = i;
-            }
-            else if(MAP[i][j]=='*'){
-                q.push(make_pair(i,j));
-            }
-        }
-    int answer = 1;
-    while(!dq.empty()){
-        q=BFS(q);
-        dq=dBFS(dq);
-        if(MAP[gy][gx]=='S'){
-            cout<<answer<<endl;
-            return 0;
-        }
-        ++answer;
-    }
+    int M, N;
+    cin>>N>>M;
+    int require[M];
+    vector<int> rq(N);
+    for(int i=1;i<=N;i++)
+        rq[i-1]=i;
 
-    cout<<"KAKTUS"<<endl;
+    for(int i=0;i<M;i++)
+        cin>>require[i];
+    int answer = 0;
+    for(int i=0;i<M;i++){
+        for(int j=0;j<rq.size();j++){
+            if(rq[j]==require[i]){
+                answer += j<rq.size()-j ? j:rq.size()-j;
+                vector<int> new_rq(N-i-1);
+                int idx = j+1;
+                for(int k=0;k<rq.size()-1;k++){
+                    if(idx==rq.size())
+                        idx=0;
+                    new_rq[k]=rq[idx++];
+                }
+                rq.assign(new_rq.begin(),new_rq.end());
+                break;
+            }
+        }
+    }
+    cout<<answer<<endl;
 }
