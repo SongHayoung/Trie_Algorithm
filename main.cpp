f5287e92ea426bfe78f13c5bae40ab04a5cdb5cc
SongHayoung <lovelydays95@naver.com>
2020-02-06 17:20:04 +0900

    10:00

--- main.cpp
+++ main.cpp
@@ -1,17 +1,23 @@
 #include <string>
 #include <vector>
-#include <map>
+#include <iostream>
 using namespace std;
 
-int solution(vector<vector<string>> clothes) {
-    map<string, int> myMAP;
-    map<string, int>::iterator it;
-    int answer = 1;
-    for (int i = 0; i<clothes.size(); i++) {
-        myMAP[clothes[i][1]] += 1;
+int solution(vector<int> citations) {
+    vector<int> inyoung(10001,0);
+    int max_inyoung = 0;
+    int total_inyoung = 0;
+    for(int i=0;i<citations.size();i++){
+        inyoung[citations[i]]+=1;
+        max_inyoung = max_inyoung < citations[i] ? citations[i]:max_inyoung;
+        total_inyoung += 1;
     }
-    for (it = myMAP.begin(); it != myMAP.end(); it++) {
-        answer *= it->second+1;
+    int answer = 0;
+    int cur_inyoung = total_inyoung;
+    for(int i=0;i<=max_inyoung;i++){
+        if(cur_inyoung>=i&&total_inyoung-cur_inyoung<=i)
+            answer = i;
+        cur_inyoung -= inyoung[i];
     }
-    return answer-1;
+    return answer;
 }
