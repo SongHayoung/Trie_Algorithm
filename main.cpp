9c5f84549b675c325e7f54cd2eb0dd6dc544724c
SongHayoung <lovelydays95@naver.com>
2020-02-06 17:38:36 +0900

    1:10:52

--- main.cpp
+++ main.cpp
@@ -1,86 +1,91 @@
 #include <iostream>
+#include <memory.h>
 #include <queue>
-using namespace std;
+#include <algorithm>
 
-char MAP[12][6];
+using namespace std;
+int R, C;
 int dx[4] = {0,1,0,-1};
 int dy[4] = {-1,0,1,0};
+int gx, gy;
+char MAP[50][50];
 
-void down(){
-    for(int j=0;j<6;j++){
-        for(int i=10;i>=0;i--){ //
-            if(MAP[i][j]!='.'){
-                int k = i;
-                bool flag = false;
-                while(MAP[k+1][j]=='.'){
-                    ++k;
-                    flag = true;
-                }
-                if(flag) {
-                    MAP[k][j] = MAP[i][j];
-                    MAP[i][j] = '.';
-                }
-            }
+void printMAP(){
+    for(int i=0;i<C;i++){
+        for(int j=0;j<R;j++){
+            cout<<MAP[i][j]<<" ";
         }
+        cout<<endl;
     }
+    cout<<endl<<endl;
 }
 
-bool play(){
-    bool flag = false;
-    bool visited[12][6] = {false, };
-    for(int i=0;i<12;i++){
-        for(int j=0;j<6;j++){
-            if(MAP[i][j]=='.'){
-                visited[i][j]=true;
-            }
-            else{
-                int cnt = 1;
-                queue<pair<int, int>> q;
-                queue<pair<int, int>> dq;
-                q.push(make_pair(i,j));
-                dq.push(make_pair(i,j));
-                visited[i][j]=true;
-                while(!q.empty()){
-                    int x = q.front().second;
-                    int y = q.front().first;
-                    q.pop();
-                    for(int k=0;k<4;k++){
-                        int nx = x + dx[k];
-                        int ny = y + dy[k];
-                        if(0<=nx&&nx<6 && 0<=ny&&ny<12){
-                            if(MAP[y][x]==MAP[ny][nx]&&!visited[ny][nx]){
-                                ++cnt;
-                                q.push(make_pair(ny,nx));
-                                dq.push(make_pair(ny,nx));
-                                visited[ny][nx] = true;
-                            }
-                        }
-                    }
-                }
-                if(cnt>=4){
-                    flag = true;
-                    while(!dq.empty()){
-                        int x = dq.front().second;
-                        int y = dq.front().first;
-                        dq.pop();
-                        MAP[y][x] = '.';
-                    }
+queue<pair<int,int>> BFS(queue<pair<int,int>> q){
+    queue<pair<int,int>> nq;
+    while(!q.empty()){
+        int x = q.front().second;
+        int y = q.front().first;
+        q.pop();
+        for(int i=0;i<4;i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(0<=nx&&nx<R && 0<=ny&&ny<C){
+                if(MAP[ny][nx]=='.'||MAP[ny][nx]=='S'){
+                    MAP[ny][nx]='*';
+                    nq.push(make_pair(ny,nx));
                 }
             }
         }
     }
-    return flag;
+    return nq;
+}
+queue<pair<int,int>> dBFS(queue<pair<int,int>> q){
+    queue<pair<int,int>> nq;
+    while(!q.empty()){
+        int x = q.front().second;
+        int y = q.front().first;
+        q.pop();
+        for(int i=0;i<4;i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(0<=nx&&nx<R && 0<=ny&&ny<C){
+                if(MAP[ny][nx]=='.'||MAP[ny][nx]=='D'){
+                    MAP[ny][nx]='S';
+                    nq.push(make_pair(ny,nx));
+                }
+            }
+        }
+    }
+    return nq;
 }
 int main(void){
-    for(int i=0;i<12;i++){
-        for(int j=0;j<6;j++){
+    cin>>C>>R;
+    int x, y;
+    queue<pair<int,int>> q;
+    queue<pair<int,int>> dq;
+    for(int i=0;i<C;i++)
+        for(int j=0;j<R;j++){
             cin>>MAP[i][j];
+            if(MAP[i][j]=='S') {
+                dq.push(make_pair(i,j));
+            }
+            else if(MAP[i][j]=='D'){
+                gx = j; gy = i;
+            }
+            else if(MAP[i][j]=='*'){
+                q.push(make_pair(i,j));
+            }
         }
-    }
-    int answer = 0;
-    while(play()){
+    int answer = 1;
+    while(!dq.empty()){
+        q=BFS(q);
+        dq=dBFS(dq);
+        if(MAP[gy][gx]=='S'){
+            cout<<answer<<endl;
+            return 0;
+        }
         ++answer;
-        down();
     }
-    cout<<answer<<endl;
+
+    cout<<"KAKTUS"<<endl;
 }
