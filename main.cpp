fb45a2a6d1ad07e9ad61559970deca7aa478dd1f
SongHayoung <lovelydays95@naver.com>
2020-02-06 17:22:36 +0900

    1:00:00

--- main.cpp
+++ main.cpp
@@ -1,19 +1,32 @@
-#include <string>
+#include <iostream>
 #include <vector>
+#include <string>
 #include <algorithm>
-using namespace std;
 
-bool solution(vector<string> phone_book) {
-    sort(phone_book.begin(),phone_book.end());
-    for(int i=0;i<phone_book.size()-1;i++){
-        if(phone_book[i].length()>phone_book[i+1].length())
-            continue;
-        for(int j=0;j<phone_book[i].length();j++){
-            if(phone_book[i][j]!=phone_book[i+1][j])
-                break;
-            if(j==phone_book[i].length()-1)
-                return false;
-        }
+using namespace std;
+bool compare(int n1, int n2) {
+    string s1 = to_string(n1);
+    string s2 = to_string(n2);
+    s1 += s1; s1 += s1;
+    s2 += s2; s2 += s2;
+    int idx = 0;
+    while (idx != 4) {
+        if (s1[idx] > s2[idx])
+            return true;
+        else if (s1[idx] < s2[idx])
+            return false;
+        else
+            ++idx;
     }
-    return true;
+    return n1 <= n2 ? false : true;
+}
+string solution(vector<int> numbers) {
+    sort(numbers.begin(), numbers.end(), compare);
+    if (numbers[0] == 0)
+        return "0";
+    string answer = "";
+    for (int i = 0; i < numbers.size(); i++) {
+        answer += to_string(numbers[i]);
+    }
+    return answer;
 }
