2330824ec8c00cb2d4886d98c1e70642f8660f9b
SongHayoung <lovelydays95@naver.com>
2020-02-06 17:55:37 +0900

    2:11:13

--- main.cpp
+++ main.cpp
@@ -1,22 +1,93 @@
-#include <string>
 #include <vector>
-#include <memory.h>
+#include <math.h>
+#include <queue>
 #include <iostream>
 using namespace std;
+int dx[4] = {0,1,0,-1};
+int dy[4] = {-1,0,1,0};
+int N;
+int MAXGroup;
+class INFO{
+public:
+    int from;
+    int to;
+    int cost;
+    INFO(int f, int t, int c) : from(f), to(t), cost(c) {}
+};
 
-int solution(int m, int n, vector<vector<int>> puddles) {
-    int MAP[101][101] = {0,};
-    int DP[101][101];
-    for(int i=0;i<puddles.size();i++){
-        MAP[puddles[i][1]][puddles[i][0]] = -1;}
-    DP[0][1]=1;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=m;j++){
-            if(MAP[i][j]==-1)
-                DP[i][j]=0;
-            else
-                DP[i][j] = (DP[i-1][j]+DP[i][j-1])%1000000007;
+class cmp{
+public:
+    bool operator()(INFO a, INFO b){
+        return a.cost>b.cost;
+    }
+};
+priority_queue<INFO,vector<INFO>,cmp> pq;
+vector<int> V;
+
+void makeGroup(int groupnum,vector<vector<int>> MAP,int H){
+    vector<vector<int>> Group(N,vector<int>(N));
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            if(Group[i][j]==0){
+                Group[i][j]=groupnum;
+                queue<pair<int,int>> q;
+                q.push(make_pair(i,j));
+                while(!q.empty()){
+                    int x = q.front().second;
+                    int y = q.front().first;
+                    q.pop();
+                    for(int k=0;k<4;k++){
+                        int near_x = x + dx[k];
+                        int near_y = y + dy[k];
+                        if(0<=near_x&&near_x<N && 0<=near_y&&near_y<N){
+                            if(Group[near_y][near_x]==0&&abs(MAP[near_y][near_x]-MAP[y][x])<=H){
+                                q.push(make_pair(near_y,near_x));
+                                Group[near_y][near_x]=groupnum;
+                            }
+                        }
+                    }
+                }
+                ++groupnum;
+            }
         }
     }
-    return DP[n][m];
+    MAXGroup = groupnum;
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            for(int k=1;k<=2;k++){
+                int near_x = j + dx[k];
+                int near_y = i + dy[k];
+                if(0<=near_x&&near_x<N && 0<=near_y&&near_y<N){
+                    if(Group[i][j]!=Group[near_y][near_x]){
+                        INFO info(Group[i][j],Group[near_y][near_x],abs(MAP[i][j]-MAP[near_y][near_x]));
+                        pq.push(info);
+                    }
+                }
+            }
+        }
+    }
+}
+
+int findGroup(int idx){
+    if(V[idx]==idx)
+        return idx;
+    return V[idx] = findGroup(V[idx]);
+}
+
+int solution(vector<vector<int>> land, int height) {
+    N = land.size();
+    makeGroup(1,land,height);
+    int answer = 0;
+    V.resize(MAXGroup);
+    for(int i=0;i<V.size();i++)
+        V[i]=i;
+    while(!pq.empty()){
+        if(findGroup(pq.top().to)!=findGroup(pq.top().from)){
+            answer += pq.top().cost;
+            int change = min(findGroup(pq.top().to),findGroup(pq.top().from));
+            V[findGroup(pq.top().to)] = V[findGroup(pq.top().from)] = change;
+        }
+        pq.pop();
+    }
+    return answer;
 }
