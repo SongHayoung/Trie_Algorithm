fd2f410a2b5af5549a9ca8d62bf07decc5caff29
SongHayoung <lovelydays95@naver.com>
2020-02-06 17:26:30 +0900

    2:11:00

--- main.cpp
+++ main.cpp
@@ -1,70 +1,105 @@
-
-#include <iostream>
-#include <map>
-#include <string>
+#include<iostream>
 #include <vector>
-#include <algorithm>
-
+#include <queue>
 using namespace std;
-class INFO {
+class atom {
 public:
-    string genre;
-    int play;
-    int code;
-    INFO(string g, int p, int c) : genre(g), play(p), code(c) {}
-};
-struct cmp_second {
-    bool operator()(pair<string, int> p1, pair<string, int> p2) {
-        return p1.second > p2.second;
+    int energy;
+    int way;
+    int x;
+    int y;
+    bool dead;
+    atom(int _x, int _y ,int e, int w, bool d) : x(_x), y(_y), energy(e), way(w) , dead(d){
     }
 };
-struct cmp_myclass {
-    bool operator()(INFO info1, INFO info2) {
-        if (info1.genre == info2.genre) {
-            if (info1.play == info2.play) {
-                return info1.code < info2.code;
-            }
-            else
-                return info1.play > info2.play;
+
+int main(int argc, char** argv)
+{
+    int test_case;
+    int T;
+    int N, x, y, energy, way,answer;
+    int dx[4] = { 0,0,-1,1 };
+    int dy[4] = { -1,1,0,0 };
+    cin>>T;
+
+    for(test_case = 1; test_case <= T; ++test_case)
+    {
+        cin >> N;
+        vector<atom> atoms;
+        vector<int> colision;
+        vector<vector<int>> MAP(2001, vector<int>(2001, 0));
+        queue<pair<int, int>> boom;
+        atoms.push_back(atom(0, 0, 0, 0, false));
+        answer = 0;
+        int value;
+        for (int j = 1; j <= N; j++) {
+            cin >> x >> y >> way >> energy;
+            x += 1000; y = 1000 - y;
+            MAP[y][x] = j;
+            atoms.push_back(atom(x, y, energy, way, false));
         }
-        else
-            return info1.genre < info2.genre;
-    }
-};
-vector<int> solution(vector<string> genres, vector<int> plays) {
-    map<string, int> Ranking;
-    vector<pair<string, int>>vectorRanking;
-    vector<INFO> songlist;
-    map<string,int>::iterator it;
-    vector<int> answer;
-    for (int i = 0; i < genres.size(); i++) {
-        Ranking[genres[i]] += plays[i];
-        songlist.push_back(INFO(genres[i], plays[i], i));
-    }
-    for (auto it = Ranking.begin(); it != Ranking.end(); it++) {
-        vectorRanking.emplace_back(make_pair(it->first, it->second));
-    }
-    sort(vectorRanking.begin(), vectorRanking.end(), cmp_second());
-    sort(songlist.begin(), songlist.end(),cmp_myclass());
-    map<string, int> index_info;
-    string cur_string = songlist[0].genre;
-    index_info[cur_string] = 0;
-    for (int i = 1; i < songlist.size(); i++) {
-        if (songlist[i].genre == cur_string)
-            continue;
-        cur_string = songlist[i].genre;
-        index_info[cur_string] = i;
-    }
-    for (int i = 0; i < vectorRanking.size(); i++) {
-        int cnt = 0;
-        for (int j = index_info[vectorRanking[i].first]; j < songlist.size(); j++) {
-            if (songlist[j].genre == vectorRanking[i].first&&cnt != 2) {
-                answer.push_back(songlist[j].code);
-                ++cnt;
+        for (int k = 0; k <= 2001; k++) {
+            bool flag=true;
+            for (int j = 1; j <= N; j++) {
+                if (atoms[j].dead)
+                    continue;
+                flag = false;
+                int _x = atoms[j].x + dx[atoms[j].way];
+                int _y = atoms[j].y + dy[atoms[j].way];
+                if (0 <= _x && _x <= 2000 && 0 <= _y && _y <= 2000) {
+                    if (MAP[_y][_x] !=0)
+                        if (atoms[j].way + atoms[MAP[_y][_x]].way == 1 || atoms[j].way + atoms[MAP[_y][_x]].way == 5) {
+                            atoms[j].dead = true;
+                            atoms[MAP[_y][_x]].dead = true;
+                            colision.push_back(j);
+                            colision.push_back(MAP[_y][_x]);
+                            MAP[_y][_x] = 0;
+                            MAP[atoms[j].y][atoms[j].x] = 0;
+                        }
+                }
             }
-            else
+            for(int j=1;j<=N;j++){
+                if (atoms[j].dead)
+                    continue;
+                if (MAP[atoms[j].y][atoms[j].x] == j)
+                    MAP[atoms[j].y][atoms[j].x] = 0;
+                atoms[j].x = atoms[j].x + dx[atoms[j].way];
+                atoms[j].y = atoms[j].y + dy[atoms[j].way];
+                if (0 > atoms[j].x || atoms[j].x > 2000 || 0 > atoms[j].y || atoms[j].y > 2000) {
+                    atoms[j].dead = true;
+                }
+                if (atoms[j].dead)
+                    continue;
+                value = MAP[atoms[j].y][atoms[j].x];
+                if (value == 1234) {
+                    colision.push_back(j);
+                    atoms[j].dead = true;
+                }
+                else if (value == 0 || value > j) {
+                    MAP[atoms[j].y][atoms[j].x] = j;
+                }
+                else {
+                    if (value < j) {
+                        colision.push_back(MAP[atoms[j].y][atoms[j].x]);
+                        colision.push_back(j);
+                        atoms[MAP[atoms[j].y][atoms[j].x]].dead = true;
+                        atoms[j].dead = true;
+                        MAP[atoms[j].y][atoms[j].x] = 1234;   //colision
+                        boom.push(make_pair(atoms[j].y, atoms[j].x));
+                    }
+                }
+            }
+            if (flag)
                 break;
+            while (!boom.empty()) {
+                MAP[boom.front().first][boom.front().second] = 0;
+                boom.pop();
+            }
         }
+        for (int j = 0; j < colision.size(); j++)
+            answer += atoms[colision[j]].energy;
+
+        cout << "#" << test_case << " " << answer << endl;
     }
-    return answer;
+    return 0;//정상종료시 반드시 0을 리턴해야합니다.
 }
