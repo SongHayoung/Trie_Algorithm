cf2bab659738bd608ace2768c01b12b951512ec0
SongHayoung <lovelydays95@naver.com>
2020-02-06 17:46:16 +0900

    33:34

--- main.cpp
+++ main.cpp
@@ -1,30 +1,14 @@
 #include <iostream>
 #include <memory.h>
-#include <algorithm>
 using namespace std;
-int N;
-int cards[1001];
-int DP[1001];
-
-int func(int n){
-    int &ret = DP[n];
-    if(ret!=-1)
-        return ret;
-    ret = cards[n];
-    for(int i=1;i<=n/2;i++){
-        ret = max(ret,func(i)+func(n-i));
-    }
-    return ret;
-}
 
 int main(void){
     int N;
     cin>>N;
-    for(int i=1;i<=N;i++)
-        cin>>cards[i];
-    memset(DP,-1,sizeof(DP));
-    DP[0] = cards[0] = 0;
-    DP[1] = cards[1];
-    func(N);
+    int DP[N+1];
+    DP[0] = DP[1] = 1;
+    for(int i=2;i<=N;i++){
+        DP[i] = (DP[i-1] + DP[i-2]*2)%10007;
+    }
     cout<<DP[N]<<endl;
 }
