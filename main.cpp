e54df19e4a3e973908edbcb5995e21ab6b9c38b3
SongHayoung <lovelydays95@naver.com>
2020-02-06 17:37:16 +0900

    23:19

--- main.cpp
+++ main.cpp
@@ -1,77 +1,86 @@
 #include <iostream>
-#include <vector>
+#include <queue>
 using namespace std;
-long long MAP[50][5];
-int main(void) {
-    int r1, c1, r2, c2;
-    cin >> r1 >> c1 >> r2 >> c2;
-    r1 += 5000;
-    r2 += 5000;
-    c1 += 5000;
-    c2 += 5000;
-    long long move = 1;
-    long long use_moved = move;
-    long long max = 0;
-    bool flag = true;
-    int x = 5000;
-    int y = 5000;
-    for(long long input = 1; input <= 10001*10001; input++){
-        if(r1<=y&&y<=r2 && c1<=x&&x<=c2) {
-            MAP[y - r1][x - c1] = input;
-            max = max < input ? input : max;
-        }
-        if(move%2==1){
-            if(flag){
-                ++x;
-                --use_moved;
-            }
-            else{
-                --y;
-                --use_moved;
-            }
-        }
-        else{
-            if(flag){
-                --x;
-                --use_moved;
-            }
-            else{
-                ++y;
-                --use_moved;
-            }
-        }
-        if(use_moved==0){
-            if(flag){
-                flag = false;
-                use_moved = move;
-            }
-            else{
-                ++move;
-                flag = true;
-                use_moved = move;
-            }
-        }
-    }
-    int cnt = 0;
-    while(max>=10){
-        max /= 10;
-        ++cnt;
-    }
-    for(int i=0;i<=r2-r1;i++){
-        for(int j=0;j<=c2-c1;j++){
-            int space = cnt;
-            int temp = MAP[i][j];
-            while(temp>=10){
-                --space;
-                temp /= 10;
-            }
-            for(int k=0;k<space;k++)
-                cout<<" ";
 
-            cout<<MAP[i][j];
-            if(j!=c2-c1)
-                cout<<" ";
+char MAP[12][6];
+int dx[4] = {0,1,0,-1};
+int dy[4] = {-1,0,1,0};
+
+void down(){
+    for(int j=0;j<6;j++){
+        for(int i=10;i>=0;i--){ //
+            if(MAP[i][j]!='.'){
+                int k = i;
+                bool flag = false;
+                while(MAP[k+1][j]=='.'){
+                    ++k;
+                    flag = true;
+                }
+                if(flag) {
+                    MAP[k][j] = MAP[i][j];
+                    MAP[i][j] = '.';
+                }
+            }
         }
-        cout<<endl;
     }
+}
+
+bool play(){
+    bool flag = false;
+    bool visited[12][6] = {false, };
+    for(int i=0;i<12;i++){
+        for(int j=0;j<6;j++){
+            if(MAP[i][j]=='.'){
+                visited[i][j]=true;
+            }
+            else{
+                int cnt = 1;
+                queue<pair<int, int>> q;
+                queue<pair<int, int>> dq;
+                q.push(make_pair(i,j));
+                dq.push(make_pair(i,j));
+                visited[i][j]=true;
+                while(!q.empty()){
+                    int x = q.front().second;
+                    int y = q.front().first;
+                    q.pop();
+                    for(int k=0;k<4;k++){
+                        int nx = x + dx[k];
+                        int ny = y + dy[k];
+                        if(0<=nx&&nx<6 && 0<=ny&&ny<12){
+                            if(MAP[y][x]==MAP[ny][nx]&&!visited[ny][nx]){
+                                ++cnt;
+                                q.push(make_pair(ny,nx));
+                                dq.push(make_pair(ny,nx));
+                                visited[ny][nx] = true;
+                            }
+                        }
+                    }
+                }
+                if(cnt>=4){
+                    flag = true;
+                    while(!dq.empty()){
+                        int x = dq.front().second;
+                        int y = dq.front().first;
+                        dq.pop();
+                        MAP[y][x] = '.';
+                    }
+                }
+            }
+        }
+    }
+    return flag;
+}
+int main(void){
+    for(int i=0;i<12;i++){
+        for(int j=0;j<6;j++){
+            cin>>MAP[i][j];
+        }
+    }
+    int answer = 0;
+    while(play()){
+        ++answer;
+        down();
+    }
+    cout<<answer<<endl;
 }
