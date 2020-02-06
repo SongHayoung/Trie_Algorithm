211d78d52b6e7abcead7b8405ab140e897af9c09
SongHayoung <lovelydays95@naver.com>
2020-02-06 17:48:54 +0900

    5:00

--- main.cpp
+++ main.cpp
@@ -1,29 +1,12 @@
 #include <iostream>
-#include <memory.h>
 using namespace std;
-int DP[500][500];
-int triangle[500][500];
-int N;
-
-int func(int x,int y){
-    if(y==N-1){
-        return triangle[y][x];
-    }
-    int &ret = DP[y][x];
-    if(ret!=-1)
-        return ret;
-    ret = triangle[y][x];
-    ret += max(func(x,y+1),func(x+1,y+1));
-    return ret;
-}
-
 int main(void){
+    int N;
     cin>>N;
-    for(int i=0;i<N;i++){
-        for(int j=0;j<=i;j++){
-            cin>>triangle[i][j];
-        }
+    int DP[N+1];
+    DP[0] = DP[1] = 1;
+    for(int i=2;i<=N;i++){
+        DP[i]=(DP[i-1]+DP[i-2])%10007;
     }
-    memset(DP,-1,sizeof(DP));
-    cout<<func(0,0)<<endl;
+    cout<<DP[N]<<endl;
 }
