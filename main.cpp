d3123be75b9e822d9ba3d20630ec9153b45a850e
SongHayoung <lovelydays95@naver.com>
2020-02-06 16:46:44 +0900

    20:00

--- main.cpp
+++ main.cpp
@@ -1,33 +1,17 @@
-#include<iostream>
-#include <math.h>
+#include <string>
+#include <vector>
+#include <map>
 using namespace std;
-char MAP[20][20];
-int dx[4] = {0,1,0,-1};
-int dy[4] = {-1,0,1,0};
-int R,C;
-bool visit[26] = {false, };
-int DFS(int x,int y){
-    int ret = 1;
-    for(int i=0;i<4;i++){
-        int next_x = x + dx[i];
-        int next_y = y + dy[i];
-        if(0<=next_x&&next_x<C&&0<=next_y&&next_y<R){
-            if(!visit[MAP[next_y][next_x]-'A']){
-                visit[MAP[next_y][next_x]-'A'] = true;
-                ret = max(ret,DFS(next_x,next_y)+1);
-                visit[MAP[next_y][next_x]-'A']=false;
-            }
-        }
+
+int solution(vector<vector<string>> clothes) {
+    map<string, int> myMAP;
+    map<string, int>::iterator it;
+    int answer = 1;
+    for (int i = 0; i<clothes.size(); i++) {
+        myMAP[clothes[i][1]] += 1;
     }
-    return ret;
-}
-int main(void)
-{
-    cin>>R>>C;
-    for(int i=0;i<R;i++)
-        for(int j=0;j<C;j++)
-            cin>>MAP[i][j];
-    visit[MAP[0][0]-'A']=true;
-    cout<<DFS(0,0)<<endl;
-    return 0;//정상종료시 반드시 0을 리턴해야합니다.
+    for (it = myMAP.begin(); it != myMAP.end(); it++) {
+        answer *= it->second+1;
+    }
+    return answer-1;
 }
