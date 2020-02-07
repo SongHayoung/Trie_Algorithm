4132628d37fd7a24a6c9a16525562249f5b542a0
SongHayoung <lovelydays95@naver.com>
2020-02-07 22:55:13 +0900

    20:00

--- main.cpp
+++ main.cpp
@@ -1,103 +1,23 @@
-#include <iostream>
+#include <vector>
 using namespace std;
-int D,W,K;
-int film[13][20];
-int mode[13];
-
-bool check(){
-    int cnt;
-    for(int i=0;i<W;i++){
-        cnt = 1;
-        for(int j=1;j<D;j++){
-            if(mode[j]!=-1&&mode[j-1]!=-1){
-                if(mode[j]==mode[j-1])
-                    ++cnt;
-                else if(j>=D-K+1)
-                    return false;
-                else
-                    cnt = 1;
-            }
-            else if(mode[j]!=-1){
-                if(mode[j]==film[j-1][i])
-                    ++cnt;
-                else if(j>=D-K+1)
-                    return false;
-                else
-                    cnt = 1;
-            }
-            else if(mode[j-1]!=-1){
-                if(film[j][i]==mode[j-1])
-                    ++cnt;
-                else if(j>=D-K+1)
-                    return false;
-                else
-                    cnt = 1;
-            }
-            else{
-                if(film[j][i]==film[j-1][i])
-                    ++cnt;
-                else if(j>=D-K+1)
-                    return false;
-                else
-                    cnt = 1;
-            }
-            if(cnt==K)
-                break;
-
-        }
-        if(cnt!=K)
-            return false;
-    }
-    return true;
-}
-bool choose(int choosed,int idx){
-    if(choosed==0)
-        return check();
-
-    if(idx>=D||D-idx<choosed)
-        return false;
-
-    mode[idx] = 0;
-    if(choose(choosed-1,idx+1))
-        return true;
-
-    mode[idx] = 1;
-    if(choose(choosed-1,idx+1))
-        return true;
-
-    mode[idx] = -1;
-    if(choose(choosed,idx+1))
-        return true;
-
-    return false;
-}
-
-int solution(){
-    cin>>D>>W>>K;
-    for(int i=0;i<D;i++)
-        for(int j=0;j<W;j++)
-            cin>>film[i][j];
-    if(K==1)
+int NUM[20];
+int tot = 0;
+int T, size;
+int DFS(int idx,int bit_mask){
+    if(idx==size){
+        int ans = tot;
+        for(int i=0;i<idx;i++)
+            if(bit_mask&1<<i)
+                ans -= 2*NUM[i];
+        if(T == ans)
+            return 1;
         return 0;
-    for(int i=0;i<D;i++)
-        mode[i] = -1;
-    for(int i=0;i<=K;i++){
-        if(choose(i,0))
-            return i;
     }
-    return K;
+    return DFS(idx+1,bit_mask)+DFS(idx+1,bit_mask|1<<idx);
 }
-
-int main(int argc, char** argv)
-{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    int test_case;
-    int T;
-    cin>>T;
-    for(int test_case=1;test_case<=T;test_case++){
-        cout<<"#"<<test_case<<" "<<solution()<<endl;
-    }
-    return 0;//정상종료시 반드시 0을 리턴해야합니다.
+int solution(vector<int> numbers, int target) {
+    T = target; size = numbers.size();
+    for(int i=0;i<numbers.size();i++)
+        tot += NUM[i] = numbers[i];
+    return DFS(0,0);
 }
