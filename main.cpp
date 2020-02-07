a17ef7d768f38e4e0f57d25ba74ad778dc17189c
SongHayoung <lovelydays95@naver.com>
2020-02-07 21:47:35 +0900

    2:30:03_running time 127ms

--- main.cpp
+++ main.cpp
@@ -2,20 +2,48 @@
 using namespace std;
 int D,W,K;
 int film[13][20];
+int mode[13];
 
 bool check(){
     int cnt;
     for(int i=0;i<W;i++){
         cnt = 1;
         for(int j=1;j<D;j++){
-            if(film[j][i]==film[j-1][i])
-                ++cnt;
-            else if(j>=D-K+1)
-                return false;
-            else
-                cnt = 1;
+            if(mode[j]!=-1&&mode[j-1]!=-1){
+                if(mode[j]==mode[j-1])
+                    ++cnt;
+                else if(j>=D-K+1)
+                    return false;
+                else
+                    cnt = 1;
+            }
+            else if(mode[j]!=-1){
+                if(mode[j]==film[j-1][i])
+                    ++cnt;
+                else if(j>=D-K+1)
+                    return false;
+                else
+                    cnt = 1;
+            }
+            else if(mode[j-1]!=-1){
+                if(film[j][i]==mode[j-1])
+                    ++cnt;
+                else if(j>=D-K+1)
+                    return false;
+                else
+                    cnt = 1;
+            }
+            else{
+                if(film[j][i]==film[j-1][i])
+                    ++cnt;
+                else if(j>=D-K+1)
+                    return false;
+                else
+                    cnt = 1;
+            }
             if(cnt==K)
                 break;
+
         }
         if(cnt!=K)
             return false;
@@ -29,21 +57,15 @@ bool choose(int choosed,int idx){
     if(idx>=D||D-idx<choosed)
         return false;
 
-    int arr[20];
-    for(int i = 0; i < W; i++){
-        arr[i] = film[idx][i];
-        film[idx][i]=0;
-    }
+    mode[idx] = 0;
     if(choose(choosed-1,idx+1))
         return true;
 
-    for(int i=0;i<W;i++)
-        film[idx][i] = 1;
+    mode[idx] = 1;
     if(choose(choosed-1,idx+1))
         return true;
 
-    for(int i=0;i<W;i++)
-        film[idx][i] = arr[i];
+    mode[idx] = -1;
     if(choose(choosed,idx+1))
         return true;
 
@@ -55,11 +77,11 @@ int solution(){
     for(int i=0;i<D;i++)
         for(int j=0;j<W;j++)
             cin>>film[i][j];
-
     if(K==1)
         return 0;
-
-    for(int i=0;i<K;i++){
+    for(int i=0;i<D;i++)
+        mode[i] = -1;
+    for(int i=0;i<=K;i++){
         if(choose(i,0))
             return i;
     }
