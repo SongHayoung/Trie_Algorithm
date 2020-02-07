fffeca706e7ecfb2de87ecf90eb1feb564377d82
SongHayoung <lovelydays95@naver.com>
2020-02-08 00:38:57 +0900

    20:00 running time 16ms

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ int dp(int idx,int bit_mask){
     }
     return dp(idx+1,bit_mask);
 }
+
 int DFS(int idx,int bit_mask){
     if(idx==size){
         if(T == tot + dp(0,bit_mask))
@@ -21,9 +22,40 @@ int DFS(int idx,int bit_mask){
     }
     return DFS(idx+1,bit_mask)+DFS(idx+1,bit_mask|1<<idx);
 }
+
 int solution(vector<int> numbers, int target) {
     T = target; size = numbers.size();
     for(int i=0;i<numbers.size();i++)
         tot += NUM[i] = numbers[i];
     return DFS(0,0);
 }
+
+/*
+ * 포인터 처리
+#include <vector>
+using namespace std;
+vector<int> *NUM;
+int *Target;
+int answer;
+void DFS(int sum,int n) {
+    if(n >= NUM->size()){
+        if(sum == *Target) answer++;
+        return;
+    }
+
+    DFS(sum , n+1);
+    DFS(sum - NUM->at(n)*2, n+1);
+}
+
+int solution(vector<int> numbers, int target) {
+    int total = 0;
+    for(int i=0;i<numbers.size();i++)
+        total += numbers[i];
+    NUM = &numbers;
+    Target = &target;
+    DFS(total , 1);
+    DFS(total-2*numbers[0], 1);
+
+    return answer;
+}
+ */
