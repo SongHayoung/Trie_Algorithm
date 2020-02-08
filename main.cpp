644d3c2a62a40a861c69c3a9c994aded1cfe981d
SongHayoung <lovelydays95@naver.com>
2020-02-09 00:17:00 +0900

    최적화9ms

--- main.cpp
+++ main.cpp
@@ -5,53 +5,43 @@ using namespace std;
 int number[10]={0,};
 int answer = 0,size;
 int cur_size;
-void calc(int d,string &s,vector<bool> &Primes){
+void calc(int d,int &s,vector<bool> &Primes){
     if(d==cur_size){
-        if((atoi(s.c_str())&1)==0)
-            return ;
-        if(Primes[atoi(s.c_str())/2]){
+        if((s&1)&Primes[s/2])
             ++answer;
-        }
         return ;
     }
     for(int i=0;i<=9;i++){
         if(number[i]!=0){
-            --number[i];
-            s[d] = i + 48;
+            --number[i];    s *=10 + i;
             calc(d + 1, s, Primes);
-            ++number[i];
+            s /= 10;    ++number[i];
         }
     }
 }
 
 int solution(string numbers) {
     size = numbers.length();
-    string s;
     int MAXLEN = pow(10,size);
     vector<bool> Primes(MAXLEN/2,true);
-    for(int i=0;i<size;i++){
-        s+=" ";
-        ++number[numbers[i]-'0'];
-    }
+    for(int i=0;i<size;i++)
+        ++number[numbers[i]-48];
+
     Primes[0] = false;
-    for(int i=3;i<MAXLEN;i+=2){
+    for(long long i=3;i*i<MAXLEN;i+=2){
         if(!Primes[i/2])
             continue;
-        for(int j=i*3;j<MAXLEN;j+=i*2){
+        for(long long j=i*i;j<MAXLEN;j+=i*2)
             Primes[j/2] = false;
-        }
     }
     if(number[2]>0)
         ++answer;
-    for(cur_size=1;cur_size<=size;cur_size++) {
-        for (int i = 1; i <= 9; i++) {
+    for(cur_size=1;cur_size<=size;cur_size++)
+        for (int i = 1; i <= 9; i++)
             if (number[i] != 0) {
                 --number[i];
-                s[0] = i + 48;
-                calc(1, s, Primes);
+                calc(1, i, Primes);
                 ++number[i];
             }
-        }
-    }
     return answer;
 }
