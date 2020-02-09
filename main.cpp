d91e60fc2457cca28751ce7413fd6c23c0d45dff
SongHayoung <lovelydays95@naver.com>
2020-02-09 17:08:01 +0900

    최적화 0.02ms

--- main.cpp
+++ main.cpp
@@ -1,47 +1,65 @@
-#include <string>
 #include <vector>
-#include <math.h>
+#include <algorithm>
 using namespace std;
-int number[10]={0,};
-int answer = 0,size;
-int cur_size;
-void calc(int d,int &s,vector<bool> &Primes){
-    if(d==cur_size){
-        if((s&1)&Primes[s/2])
-            ++answer;
-        return ;
+int b[3], d[3];
+int strike = 0, ball = 0;
+vector<int> possible;
+bool cmp(vector<int> v1, vector<int> v2){
+    if(v1[1]==v2[1])
+        return v1[2]>v2[2];
+    return v1[1]>v2[1];
+}
+inline bool is_Possible(vector<int> &baseball){
+    strike = 0, ball = 0;
+    for(int f=0;f<3;f++){
+        for(int l=0;l<3;l++){
+            if((b[f]==d[l]) && f!=l)
+                ++ball;
+            else if((b[f]==d[l]) && f==l)
+                ++strike;
+        }
+        if(strike>baseball[1]||ball>baseball[2])
+            return false;
     }
-    for(int i=0;i<=9;i++){
-        if(number[i]!=0){
-            --number[i];    s = s * 10 + i;
-            calc(d + 1, s, Primes);
-            ++number[i];    s /= 10;
+    if(strike == baseball[1] && ball == baseball[2])
+        return true;
+    return false;
+}
+int solution(vector<vector<int>> baseball) {
+    possible.reserve(1000);
+    //sort(baseball.begin(),baseball.end(),cmp);
+    b[0] = baseball[0][0]/100;
+    b[1] = (baseball[0][0]/10)%10;
+    b[2] =  baseball[0][0]%10;
+    for(int i=1;i<=9;i++){
+        for(int j=1;j<=9;j++){
+            if(i==j)
+                continue;
+            for(int k=1;k<=9;k++){
+                if(j==k||i==k)
+                    continue;
+                d[0] = i, d[1] = j, d[2] = k;
+                if(is_Possible(baseball[0])){
+                    possible.push_back(i*100+j*10+k);
+                }
+            }
         }
     }
-}
+    for(int i=1;i<baseball.size();i++){
+        for(int j=0;j<possible.size();j++){
+            strike = 0, ball = 0;
+            b[0] = baseball[i][0]/100;
+            b[1] = (baseball[i][0]/10)%10;
+            b[2] = baseball[i][0]%10;
+            d[0] = possible[j]/100;
+            d[1] = (possible[j]/10)%10;
+            d[2] = possible[j]%10;
 
-int solution(string numbers) {
-    size = numbers.length();
-    int MAXLEN = pow(10,size);
-    vector<bool> Primes(MAXLEN/2,true);
-    for(int i=0;i<size;i++)
-        ++number[numbers[i]-48];
-
-    Primes[0] = false;
-    for(long long i=3;i*i<MAXLEN;i+=2){
-        if(!Primes[i/2])
-            continue;
-        for(long long j=i*i;j<MAXLEN;j+=i*2)
-            Primes[j/2] = false;
-    }
-    if(number[2]>0)
-        ++answer;
-    for(cur_size=1;cur_size<=size;cur_size++)
-        for (int i = 1; i <= 9; i++)
-            if (number[i] != 0) {
-                --number[i];
-                calc(1, i, Primes);
-                ++number[i];
+            if(!is_Possible(baseball[i])){
+                possible.erase(possible.begin()+j);
+                --j;
             }
-    return answer;
+        }
+    }
+    return possible.size();
 }
