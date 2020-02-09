77d06eac226aa09ec2df01f0211eb0e7ee806814
SongHayoung <lovelydays95@naver.com>
2020-02-09 17:14:03 +0900

    최적화 0.02ms

--- main.cpp
+++ main.cpp
@@ -5,24 +5,23 @@ int b[3], d[3];
 int strike = 0, ball = 0;
 vector<int> possible;
 bool cmp(vector<int> v1, vector<int> v2){
-    if(v1[1]==v2[1])
-        return v1[2]>v2[2];
+    if(v1[1]==v2[1])    return v1[2]>v2[2];
     return v1[1]>v2[1];
 }
 inline bool is_Possible(vector<int> &baseball){
     strike = 0, ball = 0;
     for(int f=0;f<3;f++){
-        for(int l=0;l<3;l++){
-            if((b[f]==d[l]) && f!=l)
-                ++ball;
-            else if((b[f]==d[l]) && f==l)
-                ++strike;
+        for(int l=0;l<3;l++) {
+            if (b[f] == d[l]) {
+                if (f != l)
+                    ++ball;
+                else
+                    ++strike;
+            }
+            //if(ball>baseball[2]||strike>baseball[1])  return false;
         }
-        if(strike>baseball[1]||ball>baseball[2])
-            return false;
     }
-    if(strike == baseball[1] && ball == baseball[2])
-        return true;
+    if(strike == baseball[1] && ball == baseball[2])    return true;
     return false;
 }
 int solution(vector<vector<int>> baseball) {
