051223e4a61b09f4f3c20dcfa15a9f6b05aa601e
SongHayoung <lovelydays95@naver.com>
2020-02-10 16:27:20 +0900

    4kb 3ms

--- main.cpp
+++ main.cpp
@@ -1,59 +1,12 @@
-#include <iostream>
-#include <memory.h>
-using namespace std;
-bool NorW[20];//north == true, west = false;
-int cnt;
-char c;
-void solution(){
-    memset(NorW,false,sizeof(NorW));
-    cnt = 0;
-    while(1){
-        c=getchar();
-        if(c=='\n')
-            break;
-        getchar();getchar();getchar();
-        if(c=='n'){
-            getchar();
-            NorW[cnt] = true;
-        }
-        ++cnt;
-    }
-    long long answer = 0;
-    long long div = 1<<cnt-1;
-    long long multi = 1<<cnt-2;
-    if(!NorW[cnt-1])
-        answer = 90 * div;
-    for(int i = cnt-2; i>=0; i--){
-        if(NorW[i])
-            answer = answer - 90*multi;
-        else
-            answer = answer + 90*multi;
-        multi>>=1;
-    }
-    if(answer<0)
-        answer *= -1;
-    if(answer%div==0){
-        cout<<answer/div<<endl;
-        return ;
-    }
-    div>>=1;
-    while(1){
-        if(answer%div==0){
-            cout<<answer/div<<"/"<<((1<<cnt-1)/div)<<endl;
-            return ;
-        }
-        div>>=1;
-    }
-}
+#include <stdio.h>
 int main(int argc, char** argv)
 {
-    int test_case;
-    int T;
-    cin>>T;
-    getchar();
+    int test_case, T;
+    long long n;
+    scanf("%d",&T);
     for(test_case = 1; test_case <= T; ++test_case){
-        cout<<"#"<<test_case<<" ";
-        solution();
+        scanf("%ld",&n);
+        printf("#%d %ld %ld\n",test_case,(n-1)*(n-1)<<1^1,(n*n<<2)-1);
     }
     return 0;
 }
