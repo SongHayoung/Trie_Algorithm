9be681b3cae6c823de5918ae5229886216d57f7a
SongHayoung <lovelydays95@naver.com>
2020-02-10 16:30:49 +0900

    4kb 3ms

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char** argv)
     scanf("%d",&T);
     for(test_case = 1; test_case <= T; ++test_case){
         scanf("%ld",&n);
-        printf("#%d %ld %ld\n",test_case,(n-1)*(n-1)<<1^1,(n*n<<2)-1);
+        printf("#%d %ld %ld\n",test_case,(n-1)*(n-1)<<1^1,(n*n<<1)-1);
     }
     return 0;
 }
