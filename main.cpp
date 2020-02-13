137810eba77bd8a9127c6428f22ddf64c4af2b82
SongHayoung <lovelydays95@naver.com>
2020-02-13 18:30:35 +0900

    sort algorithms

--- main.cpp
+++ main.cpp
@@ -22,6 +22,82 @@ inline void PrintArray(int *Array,int &swaped, string s){
     printf("\nSwap Count : %d\n\n",swaped);
 }
 
+inline void Merge(int *Array, int left, int mid, int right, int &cntOfswap){
+    int sorted[right-left+1];
+    int L = left, R = mid + 1, Index = 0;
+    while(L<=mid && R<=right){
+        if(Array[L] < Array[R])
+            sorted[Index++] = Array[L++];
+        else
+            sorted[Index++] = Array[R++];
+        ++cntOfswap;
+    }
+    if(L<=mid){
+        for(;L<=mid;){
+            sorted[Index++] = Array[L++];
+            ++cntOfswap;
+        }
+    }
+    else{
+        for(;R<=right;){
+            sorted[Index++] = Array[R++];
+            ++cntOfswap;
+        }
+    }
+    for(Index = left; Index <= right; Index++)
+        Array[Index] = sorted[Index - left];
+}
+
+void MergeSort(int *Array, int left, int right, int &cntOfswap){
+    if(left < right){
+        int mid = (left + right)/2;
+        MergeSort(Array,left,mid,cntOfswap);
+        MergeSort(Array,mid+1,right,cntOfswap);
+        Merge(Array, left, mid, right,cntOfswap);
+    }
+}
+
+inline void CellSort(int *Array, int &_start, int &Gap, int &cntOfswap){
+    for(int start = _start + Gap; start < MAXSIZE; start += Gap){
+        int temp = Array[start];
+        int compare = start - Gap;
+        for(;compare>=_start && Array[compare] > temp; compare -= Gap) {
+            Array[compare + Gap] = Array[compare];
+            ++cntOfswap;
+        }
+        Array[compare+Gap] = temp;
+    }
+}
+
+
+inline void Partition(int *Array, int &left, int &right, int &high, int &cntOfswap){
+    int low = left;
+    int pivot = Array[left];
+    do{
+        do{
+            low++;
+        }while(low<=right && Array[low]<pivot);
+        do{
+            high--;
+        }while(high>=left && Array[high]>pivot);
+        if(low<high) {
+            SWAP(Array[low], Array[high], TYPE);
+            ++cntOfswap;
+        }
+
+    }while(low<high);
+    SWAP(Array[left],Array[high],TYPE);
+    ++cntOfswap;
+}
+
+void QuickSort(int *Array, int left, int right, int &cntOfswap){
+    if(left<right){
+        int standard = right+1;
+        Partition(Array,left,right,standard,cntOfswap);
+        QuickSort(Array,left,standard-1,cntOfswap);
+        QuickSort(Array,standard+1,right,cntOfswap);
+    }
+}
 
 void BubbleSort_Simulation(){
     int BubblesortedArray[MAXSIZE];
@@ -76,41 +152,6 @@ void InsertionSort_Simulation(){
     PrintArray(InsertionsortedArray,cntOfswap,"INSERTION SORT\n");
 }
 
-inline void Merge(int *Array, int left, int mid, int right, int &cntOfswap){
-    int sorted[right-left+1];
-    int L = left, R = mid + 1, Index = 0;
-    while(L<=mid && R<=right){
-        if(Array[L] < Array[R])
-            sorted[Index++] = Array[L++];
-        else
-            sorted[Index++] = Array[R++];
-        ++cntOfswap;
-    }
-    if(L<=mid){
-        for(;L<=mid;){
-            sorted[Index++] = Array[L++];
-            ++cntOfswap;
-        }
-    }
-    else{
-        for(;R<=right;){
-            sorted[Index++] = Array[R++];
-            ++cntOfswap;
-        }
-    }
-    for(Index = left; Index <= right; Index++)
-        Array[Index] = sorted[Index - left];
-}
-
-void MergeSort(int *Array, int left, int right, int &cntOfswap){
-    if(left < right){
-        int mid = (left + right)/2;
-        MergeSort(Array,left,mid,cntOfswap);
-        MergeSort(Array,mid+1,right,cntOfswap);
-        Merge(Array, left, mid, right,cntOfswap);
-    }
-}
-
 void MergeSort_Simulation(){
     int MergesortedArray[MAXSIZE];
     int cntOfswap = 0;
@@ -137,18 +178,6 @@ void GnomeSort_Simulation(){
     PrintArray(GnomesortedArray,cntOfswap,"GNOME SORT\n");
 }
 
-inline void CellSort(int *Array, int &_start, int &Gap, int &cntOfswap){
-    for(int start = _start + Gap; start < MAXSIZE; start += Gap){
-        int temp = Array[start];
-        int compare = start - Gap;
-        for(;compare>=_start && Array[compare] > temp; compare -= Gap) {
-            Array[compare + Gap] = Array[compare];
-            ++cntOfswap;
-        }
-        Array[compare+Gap] = temp;
-    }
-}
-
 void CellSort_Simulation(){
     int CellsortedArray[MAXSIZE];
     int cntOfswap = 0;
@@ -163,34 +192,6 @@ void CellSort_Simulation(){
         Gap>>=1;
     }
     PrintArray(CellsortedArray,cntOfswap,"CELL SORT\n");
-}
-inline void Partition(int *Array, int &left, int &right, int &high, int &cntOfswap){
-    int low = left;
-    int pivot = Array[left];
-    do{
-        do{
-            low++;
-        }while(low<=right && Array[low]<pivot);
-        do{
-            high--;
-        }while(high>=left && Array[high]>pivot);
-        if(low<high) {
-            SWAP(Array[low], Array[high], TYPE);
-            ++cntOfswap;
-        }
-
-    }while(low<high);
-    SWAP(Array[left],Array[high],TYPE);
-    ++cntOfswap;
-}
-
-void QuickSort(int *Array, int left, int right, int &cntOfswap){
-    if(left<right){
-        int standard = right+1;
-        Partition(Array,left,right,standard,cntOfswap);
-        QuickSort(Array,left,standard-1,cntOfswap);
-        QuickSort(Array,standard+1,right,cntOfswap);
-    }
 }
 
 void QuickSort_Simulation(){
